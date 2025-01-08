d1faa070ed37f7cef7eb00b13a24329f2614012a
Eddie Lam <el@topo.cc>
2025-01-08 13:26:10 +0800

    fix: opening closed stream

--- presentation/src/main/java/BooksController.java
+++ presentation/src/main/java/BooksController.java
@@ -24,26 +24,29 @@ public class BooksController extends ControllerBase {
     }
 
     public void handlePostBooks(HttpExchange httpExchange) throws IOException, SQLException {
-        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
-        BufferedReader br = new BufferedReader(isr);
 
-        // From now on, the right way of moving from bytes to utf-8 characters:
-
-        int b;
-        StringBuilder buf = new StringBuilder(512);
-        while ((b = br.read()) != -1) {
-            buf.append((char) b);
+        try (
+            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+        ) {
+            // From now on, the right way of moving from bytes to utf-8 characters:
+            int b;
+            StringBuilder buf = new StringBuilder(512);
+            while ((b = br.read()) != -1) {
+                buf.append((char) b);
+            }
+    
+            br.close();
+            isr.close();
+    
+            String json = buf.toString();
+    
+            // parse to object
+            CreateBookDto dto = parse(json, CreateBookDto.class);
+            int rowsAffected = this.booksService.create(dto);
+    
+            sendJson(httpExchange, null, rowsAffected > 0 ? 204 : 400);
         }
 
-        br.close();
-        isr.close();
-
-        String json = buf.toString();
-
-        // parse to object
-        CreateBookDto dto = parse(json, CreateBookDto.class);
-        int rowsAffected = this.booksService.create(dto);
-
-        sendJson(httpExchange, null, rowsAffected > 0 ? 204 : 400);
     }
 }
--- presentation/src/main/java/ControllerBase.java
+++ presentation/src/main/java/ControllerBase.java
@@ -14,26 +14,25 @@ public class ControllerBase {
         return objectMapper.readValue(json, c);
     }
 
-    protected static final void sendJson(HttpExchange httpExchange, Object data, int status) {
+    protected static final void sendJson(HttpExchange httpExchange, Object data, int status) throws IOException {
         sendResponse(httpExchange, "application/json", data, status);
     }
 
-    protected static final void sendResponse(HttpExchange httpExchange, String contentType, Object data, int status) {
-        try (
-                OutputStream outputStream = httpExchange.getResponseBody()
-        ) {
-            // 1. set res headers
-            String text = parseResponseData(data, contentType);
+    protected static final void sendResponse(HttpExchange httpExchange, String contentType, Object data, int status) throws IOException {
+        
+        // 1. set res headers
+        String text = parseResponseData(data, contentType);
+        
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.set("Content-Type", contentType);
+        httpExchange.sendResponseHeaders(status, text.length());
+        
+        // 2. write response body
+        OutputStream outputStream = httpExchange.getResponseBody();
+        outputStream.write(text.getBytes());
 
-            Headers headers = httpExchange.getResponseHeaders();
-            headers.set("Content-Type", contentType);
-            httpExchange.sendResponseHeaders(status, text.length());
-            
-            // 2. write response body
-            outputStream.write(text.getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        outputStream.close();
+        httpExchange.close();
     }
 
     private static String parseResponseData(Object data, String type) throws JsonProcessingException {
